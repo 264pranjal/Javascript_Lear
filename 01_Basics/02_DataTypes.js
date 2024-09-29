10454586c634223a9668f945714e09867cdf80b5
Pranjal Agrawal <37728750+264pranjal@users.noreply.github.com>
2024-09-29 17:04:37 +0000

    new changes

--- 01_Basics/02_DataTypes.js
+++ 01_Basics/02_DataTypes.js
@@ -2,6 +2,23 @@
 
 //alert (3+3)  // we are using nodejs, not browser
 
-console.log(3 
-    + 
-    3); // code readability should be high
+console.log(3 + 3); // code readability should be high
+
+let name = "Pranjal";
+let age = 18;
+let isLoggedIn = false;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => value assign nhi hui hai 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+
